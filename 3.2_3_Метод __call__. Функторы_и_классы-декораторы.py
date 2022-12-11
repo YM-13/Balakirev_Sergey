469008a84f5431481c3abbfe1e6f484626d60ee7
YM <exitospec1@gmail.com>
2022-12-10 20:05:44 -0600

    10.12.22 - 1

--- 3.2_3_Метод __call__. Функторы_и_классы-декораторы.py
+++ 3.2_3_Метод __call__. Функторы_и_классы-декораторы.py
@@ -1,11 +1,14 @@
-
-
 class ImageFileAcceptor:
 	def __init__(self, extensions):
 		self.extensions = extensions
 
 	def __call__(self, filenames):
 		return any(map(lambda x: filenames.endswith(x), self.extensions))
+
+	# def __call__(self, *args, **kwargs):
+	# 	self.filenames = args[0]
+	# 	from_index = self.filenames.rfind(".")
+	# 	return self.filenames[from_index + 1:] in self.extensions
 
 
 filenames = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.png"]
@@ -14,6 +17,4 @@ acceptor = ImageFileAcceptor(('jpg', 'bmp', 'jpeg'))
 image_filenames = filter(acceptor, filenames)
 print(list(image_filenames))  # ["boat.jpg", "ava.jpg", "forest.jpeg"]
 
-
 image_filenames = filter(acceptor, filenames)
-
